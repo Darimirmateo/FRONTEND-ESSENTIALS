1db81e26ae7203a0cb3fcc47061d6bd6c5d21daf
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-11-06 10:26:56 +0100

    vlagen nederland

---
+++ 05 - Javascript/taak05 - As if/script.js
@@ -0,0 +1,7 @@
+let x = 8;
+let y = 5;
+
+if(x == y){
+    console.log("De twee getallen zijn gelijk.");
+}
+console.log("Welke taak is het moeilijkst?");
---
+++ 05 - Javascript/taak06 - If  something else/script.js
@@ -0,0 +1,8 @@
+ let spoorBomenOmlaag = true;
+
+    if(spoorBomenOmlaag == true){
+        console.log("Ik moet helaas even wachten");
+    }
+    else{
+        console.log("Haha, Ik rijd lekker door");
+    }
